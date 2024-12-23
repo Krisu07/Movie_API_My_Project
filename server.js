d561de51494e7182eb416412dd433dc4c3363e70
Krista Korento <krista.korento@gmail.com>
2024-12-23 19:58:44 +0200

    server javascript added to "user" type mistoke fixed.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(3001, ()=>{
 app.get('/User', async (req,res) => {
 
     try {
-        const result= await pgPool.query('SELECT * FROM User');
+        const result= await pgPool.query('SELECT * FROM "User"');
         res.json(result.rows);
     } catch (error) {
         res.status(400).json({error: error.message});
