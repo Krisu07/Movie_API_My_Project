497ede5b955f6b1a6ee4b20fda15f411a17f4b62
Krista Korento <krista.korento@gmail.com>
2025-01-04 16:43:57 +0200

    Deleted error making code part.

--- server.js
+++ server.js
@@ -8,15 +8,4 @@ app.listen(3003, () => {
     console.log('Server is running on in port 3003');
 });
 
-//lisätään uusi genre 
-app.post('/genres', async (require, response) =>{
-    const {name} = require.body;
-    try{
-        const result = await pgPool.query('INSERT INTO genres (name) VALUES (1$) RETURNING*',[name]);
-        response.status(201).json(result.rows[0]);        
-    } catch (error){
-        response.status(400).json({error: error.message});
-    }
-})
-
 
