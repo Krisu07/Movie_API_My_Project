80b4e9f16053c658fb716d99996992454da315cd
Krista Korento <krista.korento@gmail.com>
2025-01-04 16:30:35 +0200

    server code added post new genre so in postman we can add new genre.

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-import express from 'express';
+import express, { response } from 'express';
 import { pgPool } from './pg_connection.js';
 
 const app = express();
-app.use(express.json());
 
+//serverin starttaus
 app.listen(3003, () => {
     console.log('Server is running on in port 3003');
 });
+
+//lisätään uusi genre 
+app.post('/genres', async (require, response) =>{
+    const {name} = require.body;
+    try{
+        const result = await pgPool.query('INSERT INTO genres (name) VALUES (1$) RETURNING*',[name]);
+        response.status(201).json(result.rows[0]);        
+    } catch (error){
+        response.status(400).json({error: error.message});
+    }
+})
 
 
